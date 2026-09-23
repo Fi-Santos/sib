ad3f613dd0cbc40a5fab5a360a6df3337d8cf68b
Fi-Santos <eduardasantos6789@gmail.com>
2026-09-23 11:14:18 +0100

    Adding my co-authorship to the package

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
-__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
+__author__ = "Eduarda Santos"
+__credits__ = ["Eduarda Santos"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__maintainer__ = "Eduarda Santos"
+__email__ = "eduardasantos6789@gmail.com"
 
 
 
