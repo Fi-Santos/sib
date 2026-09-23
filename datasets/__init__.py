fa2871675f1769cd158bbdf68fb05a4eedea6ee1
Fi-Santos <eduardasantos6789@gmail.com>
2026-09-23 11:12:34 +0100

    Adding my co-authorship to the package

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,3 +2,9 @@
 import os
 
 DATASETS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+__author__ = "Fi-Santos" 
+__credits__ = ["Fi-Santos"]
+__license__ = "Apache License 2.0"
+__version__ = "0.0.1"
+__maintainer__ = "Fi-Santos"
+__email__ = "eduardasantos@gmail.com"
